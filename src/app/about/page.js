42eed693b0d59c99fd38e6bee65fd70753e59d4a
Lucas Lima <limalucas7027@gmail.com>
2023-11-02 16:42:01 -0300

    First Commit

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import Header from './header';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Welcome to my application!</h1>
+    </div>
+  );
+};
+
+export default Home;
